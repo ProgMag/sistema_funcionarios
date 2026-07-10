359d26246a768f663ceece9718b4ad36550488c5
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-07-10 19:25:41 -0300

    Adiciona construtor

---
+++ src/Funcionario.java
@@ -0,0 +1,10 @@
+public abstract class Funcionario implements Tributavel {
+
+    private String nome;
+    private double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
