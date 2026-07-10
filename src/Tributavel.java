712ccef8783716bbd3b408e7bc16bfbc9a7c289d
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-07-10 19:18:30 -0300

    Adiciona método para calcular salario liquido

--- src/Tributavel.java
+++ src/Tributavel.java
@@ -1,4 +1,9 @@
 public interface Tributavel {
 
     double calcularImposto(double salario);
+
+    default double calcularSalarioLiquido(double salario) {
+       double imposto = calcularImposto(salario);
+       return salario - imposto;
+    }
 }
