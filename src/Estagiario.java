256b7732e3c4ba7983f9f3eb5050b12c29f04785
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-07-13 18:36:24 -0300

    Adiciona método para calcular salário final

--- src/Estagiario.java
+++ src/Estagiario.java
@@ -3,4 +3,9 @@ public class Estagiario extends Funcionario{
     public Estagiario(String nome, double salario) {
         super(nome, salario);
     }
+
+    @Override
+    public double calcularSalarioFinal() {
+        return getSalarioBase();
+    }
 }
